95460cc6b983ce62f01c717a33a4118dca9fdd52
Alan Gomes <aln.dsg@hotmail.com>
2020-12-25 21:29:25 -0300

    use reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
+const reducer = (state, action) => {
+    // state === { count: number }
+    // action === { type: 'change_Increase' || 'change_Decrease', playload: 1}
+    switch(action.type){
+        case 'change_Increase':
+            return {...state, count: state.count + action.payload};
+        case 'change_Decrease':
+            return state.count - action.payload < 0 
+            ? state
+            : {...state, count: state.count - action.payload};
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, trigger] = useReducer(reducer, {count: 0});
 
     return (
         <View>
-            <Button title="Increase" onPress={() =>{setCounter(counter + 1)}}/>
-            <Button title="Decrease" onPress={() =>{setCounter(counter - 1)}}/>
-            <Text>Current Count:{counter}</Text>
+            <Button title="Increase" onPress={() => {trigger({ type: 'change_Increase', payload: 1 })}}/>
+            <Button title="Decrease" onPress={() => {trigger({ type: 'change_Decrease', payload: 1 })}}/>
+            <Text>Current Count:{state.count}</Text>
         </View>
     );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,46 +1,45 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View, FlatList} from 'react-native';
+import React, {useReducer} from 'react';
+import {Button, StyleSheet, Text, View, FlatList, ActionSheetIOS} from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0 
+            ? state
+            : {...state, red: state.red + action.payload};
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0 
+            ? state
+            : {...state, green: state.green + action.payload};
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 
+            ? state
+            : {...state, blue: state.blue + action.payload};
+        default:
+            return state;
+    }
+};
+
 const SquareScreen = () => {
-    const [red,setRed] = useState(0);
-    const [green,setGreen] = useState(0);
-    const [blue,setBlue] = useState(0);
-
-    const setColor = (color, newValue) => {
-        if(newValue > 255 || newValue < 0){
-            return null;
-        }
-        console.log(`color ${color} = ${newValue}`);
-        switch (color){
-            case 'red':
-                setRed(newValue);   return null;
-            case 'green':
-                setGreen(newValue); return null;
-            case 'blue':
-                setBlue(newValue);  return null;
-            default:
-                return null;
-        }
-    };
-
+    const [state, trigger] = useReducer(reducer, {red: 0, green: 0, blue: 0});
     return (
         <View> 
             <ColorCounter  color="Red" 
-                onIncrease={() => setColor('red', red + COLOR_INCREMENT)}
-                OnDecrease={() => setColor('red', red - COLOR_INCREMENT)}
+                onIncrease={() => trigger({ type: 'change_red', payload : COLOR_INCREMENT })}
+                OnDecrease={() => trigger({ type: 'change_red', payload : -1 * COLOR_INCREMENT })}
             />
             <ColorCounter color="Green"
-                onIncrease={() => setColor('green', green + COLOR_INCREMENT)}
-                OnDecrease={() => setColor('green', green - COLOR_INCREMENT)}
+                onIncrease={() => trigger({ type: 'change_green', payload : COLOR_INCREMENT })}
+                OnDecrease={() => trigger({ type: 'change_green', payload :  -1 * COLOR_INCREMENT })}
             />
             <ColorCounter color="Blue"
-                onIncrease={() => setColor('blue', blue + COLOR_INCREMENT)}
-                OnDecrease={() => setColor('blue', blue - COLOR_INCREMENT)}
+                onIncrease={() => trigger({ type: 'change_blue', payload : COLOR_INCREMENT })}
+                OnDecrease={() => trigger({ type: 'change_blue', payload :  -1 * COLOR_INCREMENT })}
             />
-            <View style={[styles.squareColour,{backgroundColor: `rgb(${red},${green},${blue})`}]}></View>
+            <View style={[styles.squareColour,{backgroundColor: `rgb(${state.red},${state.green},${state.blue})`}]}></View>
         </View>
     );
 };
